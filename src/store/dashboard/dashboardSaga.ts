22350496ecbe50df745082676f1b5542f6560204
jesseliza <jess@qubicle.ai>
2025-10-07 03:52:17 +0530

    update

--- src/store/dashboard/dashboardSaga.ts
+++ src/store/dashboard/dashboardSaga.ts
@@ -1,10 +1,10 @@
 import { all, call, put, takeLatest } from 'redux-saga/effects';
-import axios from 'axios';
+import axiosInstance from "../../services/apiHelper";
 import { getDashboardData, getDashboardDataSuccess, getDashboardDataFailure } from './dashboardSlice';
 
 function* fetchDashboardData() {
   try {
-    const response = yield call(axios.get, '/api/dashboard');
+    const response = yield call(axiosInstance.get, '/api/dashboard');
     yield put(getDashboardDataSuccess(response.data.data));
   } catch (error: any) {
     yield put(getDashboardDataFailure(error.message));
